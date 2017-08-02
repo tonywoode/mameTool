386df32e0a363eef7dd0acf88984a9438f3d072f
tony woode <tonywoodecode@gmail.com>
2017-08-02 09:05:25 +0100

    most basic json output
    get minimum fields for a romdata
    print them to console & a json file

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const makeSystemsAsync = require('./src/readMameXML.js')
 const mameXMLInPath    = `./inputs/mame187.xml`
+const jsonOutPath      = `./outputs/mame.json`
+
 const fs               = require(`fs`)
 const mameXMLStream    = fs.createReadStream(mameXMLInPath)
-const pretty = systems => console.log(JSON.stringify(systems, null, `\t`))
 
-makeSystemsAsync(mameXMLStream).then( systems => pretty(systems) )
+const print = (systems) => {
+//print out the json we made, romdatamaker.js uses it
+  const pretty = JSON.stringify(systems, null, `\t`)
+  console.log(`Printing systems JSON to ${jsonOutPath}`)
+  fs.writeFileSync(jsonOutPath, pretty)
+  console.log(`done`)
+}
+
+makeSystemsAsync(mameXMLStream).then( systems => print(systems) )
