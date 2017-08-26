632d9d74f3115f4de8796b43c2f4ca129d607134
tony woode <tonywoodecode@gmail.com>
2017-08-26 10:48:20 +0100

    shortened some functions in fillFromIni

--- src/fillFromIni.js
+++ src/fillFromIni.js
@@ -1,21 +1,16 @@
 'use strict'
 
-const R = require(`ramda`)
+const {map, assoc} = require(`ramda`)
 const {loadIni} = require(`./iniReader.js`)
 
 // I think its faster to search the ini for a each mame game than to search for each ini entry in the mame json
 const getEntryFromIni = ini => call => ini[call]
 
 // pointfree takes the mame json
-const fillFromIni = (typeOfIni, ini) => {
-
-  const mameJsonFilled = R.map( game => {
-    const settingForThisGame = getEntryFromIni(ini)(game[`call`])
-    return settingForThisGame? R.assoc(typeOfIni, settingForThisGame, game) : game
-  })
-  
-  return mameJsonFilled
-}
+const fillFromIni = (typeOfIni, ini) => map( game => {
+  const settingForThisGame = getEntryFromIni(ini)(game[`call`])
+  return settingForThisGame? assoc(typeOfIni, settingForThisGame, game) : game
+})
 
 // parse, format and incorporate an ini into our mame JSON,
 //   note this works by the ini name being the same as they key in the json
