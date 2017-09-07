3a601e9d6c56da0b27a4e37f52abba0011522eec
tony woode <tonywoodecode@gmail.com>
2017-09-07 19:57:56 +0100

    tests pulled up sytax errors

--- src/types.js
+++ src/types.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const outputDir          = require(`./getDir.js`).getOutputDir()
+const outputDir          = require(`./getDir.js`).getOutputDir() //also needs changing in package.json config
 
 const {tagged}           = require('daggy')
 const Emu = tagged(`Emu`, [`EmuName`,                 `RomdataOutParDir`,          `Icon`] )
-exports.Mame         = Emu(`mame64`,                  `${outputDir}/mame`,         `mame`)
+exports.Mame         = Emu(`Mame64`,                  `${outputDir}/mame`,         `mame`)
 exports.RetroArch    = Emu(`Retroarch Arcade (Mame)`, `${outputDir}/retroarch`,    `RetroArch`)
 
 
