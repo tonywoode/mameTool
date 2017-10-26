b708da7ded288e105179d13e39ccb27fd90605d6
tony woode <tonywoodecode@gmail.com>
2017-10-26 13:22:13 +0100

    don't try to make output dir anymore, that's dev convern only

--- src/scan/arcadeScan/printJson.js
+++ src/scan/arcadeScan/printJson.js
@@ -1,12 +1,8 @@
 'use strict'
 
 let   fs                          = require('fs') //rewired in test, don't try and const or destructure
-let   mkdirp                      = require('mkdirp') //ditto
-const path                        = require('path')
 
 module.exports = jsonOutPath => systems => {
-  const outputDir = path.dirname(jsonOutPath)
-  fs.existsSync(outputDir) || mkdirp(outputDir) 
   fs.writeFileSync(jsonOutPath, JSON.stringify(systems, null, `\t`))  
   return systems
 }
