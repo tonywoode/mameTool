8b0596748f3ded6b2d49139a90cb922e047fdc5d
tony woode <tonywoodecode@gmail.com>
2018-04-16 23:01:03 +0100

    gather a list of system dat deletions

--- src/scan/datAndEfind/printSystemsDat.js
+++ src/scan/datAndEfind/printSystemsDat.js
@@ -2,10 +2,38 @@
 
 const fs             = require('fs-extra') //otherwise copying a file is non-trivial 
 const R              = require('ramda')
+const {efindBoringSystems
+  , embeddedSystemsBoringSystems
+  , systemTypesToRemove} = require('../../messConfig.json')
 
 /* Now the ini is out, print out a systems list */
 module.exports = (log, existingSystemsDat, datOutPath) => systems => {
 
+  /*has the user got any systems we've later decided are boring 
+   * TODO: there exists a slim chance the user rather than mametool added one of these,
+   * its rather unlikely */
+  
+  const systemsToDelete = efindBoringSystems.reduce( (accum, item) => {
+    existingSystemsDat.includes(item) && accum.push(item)
+    return accum
+  },[])
+
+  console.log(`first round deletions: ` + systemsToDelete.toString() )
+
+  const systemsToDeletePlusEmbedded = embeddedSystemsBoringSystems.reduce( (accum, item) => {
+    existingSystemsDat.includes(item) && accum.push(item)
+    return accum
+  }, systemsToDelete)
+
+  console.log(`second round deletions: ` + systemsToDeletePlusEmbedded.toString() )
+
+  const systemsToDeletePlusTypesToRemove = systemTypesToRemove.reduce( (accum, item) => {
+    existingSystemsDat.includes(item) && accum.push(item)
+    return accum
+  }, systemsToDeletePlusEmbedded)
+
+  console.log(`third round deletions: ` + systemsToDeletePlusTypesToRemove.toString() )
+   
     const lister = R.pipe( R.map( ({systemType}) => (`${systemType}`) ), R.uniq)(systems)
     const ordered = lister.sort( (a, b) => a.localeCompare(b) )
   
@@ -14,6 +42,8 @@ module.exports = (log, existingSystemsDat, datOutPath) => systems => {
     const orderedUnionDat = unionDat.sort( (a, b) => a.localeCompare(b) )
     const joinedUnionDat  = orderedUnionDat.join(`\n`) 
    
+ 
+
     console.log(`Printing systems dat to ${datOutPath}`)
     if (log.dat) console.log(joinedUnionDat)
     //i don't dare to overwrite the users systems.dat the first time
