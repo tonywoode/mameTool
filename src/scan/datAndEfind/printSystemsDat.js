0d233505b10c3eded7cb754301fee5f94c5939e8
tony woode <tonywoodecode@gmail.com>
2018-04-17 18:28:14 +0100

    demonstrably filtering systemsDat for unwanted names...
    ...with a native imp for comparison

--- src/scan/datAndEfind/printSystemsDat.js
+++ src/scan/datAndEfind/printSystemsDat.js
@@ -9,46 +9,41 @@ const {efindBoringSystems
 /* Now the ini is out, print out a systems list */
 module.exports = (log, existingSystemsDat, datOutPath) => systems => {
 
-  /*has the user got any systems we've later decided are boring 
+  /*has the user got any systems we've later decided are boring? 
+   * Go through the boring and old systems in mameTools config file, excluding them from the systems dat
+   * Uses list from messConfig to removes unwanted system names that may have previously been printed to the users systems.dat. 
    * TODO: there exists a slim chance the user rather than mametool added one of these,
    * its rather unlikely */
-  
-  const systemsToDelete = efindBoringSystems.reduce( (accum, item) => {
-    existingSystemsDat.includes(item) && accum.push(item)
-    return accum
-  },[])
 
-  console.log(`first round deletions: ` + systemsToDelete.toString() )
+  // Each of these systems needs to be used to filter the systems.dat
+  const sections = [ efindBoringSystems, embeddedSystemsBoringSystems, systemTypesToRemove ]
 
-  const systemsToDeletePlusEmbedded = embeddedSystemsBoringSystems.reduce( (accum, item) => {
-    existingSystemsDat.includes(item) && accum.push(item)
-    return accum
-  }, systemsToDelete)
+  // ::[a] => a => bool - does system name appear in list (equals is curried a system from systems)
+  const isItInExclusionList = systems => system => R.none(R.equals(system), systems)
+  //::[a] => [a] => [a] - (change filter to reject to debug)
+  const filterSystemsDat = exclusions => R.filter(isItInExclusionList(exclusions), existingSystemsDat)
+  const filteredExistingSystemsDat = R.reduce( (accum, item) => filterSystemsDat(item), [], sections)
+  console.log(filteredExistingSystemsDat)
 
-  console.log(`second round deletions: ` + systemsToDeletePlusEmbedded.toString() )
+  //native js version of the same....
+  //const filterSystemsDat = (source, sections) => source.filter(x => !sections.some(e => e.indexOf(x) !== -1))
+  //const filteredExistingSystemsDat = filterSystemsDat(existingSystemsDat, sections)
+  //console.log(filteredExistingSystemsDat)
+  process.exit()
 
-  const systemsToDeletePlusTypesToRemove = systemTypesToRemove.reduce( (accum, item) => {
-    existingSystemsDat.includes(item) && accum.push(item)
-    return accum
-  }, systemsToDeletePlusEmbedded)
+  const lister = R.pipe( R.map( ({systemType}) => (`${systemType}`) ), R.uniq)(systems)
+  const ordered = lister.sort( (a, b) => a.localeCompare(b) )
 
-  console.log(`third round deletions: ` + systemsToDeletePlusTypesToRemove.toString() )
-   
-    const lister = R.pipe( R.map( ({systemType}) => (`${systemType}`) ), R.uniq)(systems)
-    const ordered = lister.sort( (a, b) => a.localeCompare(b) )
-  
-    //make the union dat of the old quickplay and the new systems dat
-    const unionDat        = R.union(existingSystemsDat, ordered)
-    const orderedUnionDat = unionDat.sort( (a, b) => a.localeCompare(b) )
-    const joinedUnionDat  = orderedUnionDat.join(`\n`) 
-   
- 
+  //make the union dat of the old quickplay and the new systems dat
+  const unionDat        = R.union(existingSystemsDat, ordered)
+  const orderedUnionDat = unionDat.sort( (a, b) => a.localeCompare(b) )
+  const joinedUnionDat  = orderedUnionDat.join(`\n`) 
 
-    console.log(`Printing systems dat to ${datOutPath}`)
-    if (log.dat) console.log(joinedUnionDat)
-    //i don't dare to overwrite the users systems.dat the first time
-    fs.existsSync(datOutPath) && fs.copySync(datOutPath, `${datOutPath}.old`)
-    fs.writeFileSync(datOutPath, joinedUnionDat, `latin1`)  //utf8 isn't possible at this time
+  console.log(`Printing systems dat to ${datOutPath}`)
+  if (log.dat) console.log(joinedUnionDat)
+  //i don't dare to overwrite the users systems.dat the first time
+  fs.existsSync(datOutPath) && fs.copySync(datOutPath, `${datOutPath}.old`)
+  fs.writeFileSync(datOutPath, joinedUnionDat, `latin1`)  //utf8 isn't possible at this time
 
-    return systems
+  return systems
 }
